de9c6b4df669829f8a9bde7ca43b3a7dcd81bf0c
ANKIT KUMAR VISHWAKARMA <68489896+ankitkvishwakarma@users.noreply.github.com>
2025-08-10 21:59:35 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import "./index.css"
 import { BrowserRouter } from 'react-router-dom'; // ✅ Import router
-import { Provider } from 'react-redux';
-import store from './redux/store';
+import { Provider,store } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
